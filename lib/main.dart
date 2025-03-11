5cc5bbd3c6b18af4eca8cc78c7aa707ac3201370
Brian <aladebrian@gmail.com>
2025-03-11 17:24:44 -0400

    Added Amount subclass in recipes.dart, added tag functionality for favorite and saved iconbuttons, improved the recipetile widget (function) to include formatted image, centered title, and trailing iconbuttons

--- lib/main.dart
+++ lib/main.dart
@@ -36,31 +36,41 @@ class _MyHomePageState extends State<MyHomePage> {
     Recipe(
       name: "Recipe 1",
       steps: ["do this", 'then this', 'finally this'],
-      ingredients: {"a little bit of this": 1, 'a little bit of that': 2},
-      tags: HashSet.from(["favorite"]),
+      ingredients: {
+        "a little bit of this": Amount(number: 1, unit: 'quart'),
+        'a little bit of that': Amount(number: 0.5),
+      },
+      tags: HashSet.from(["favorite", "vegan"]),
     ),
     Recipe(
       name: "Recipe 2",
       steps: ["do this again", 'then this', 'unfortunately this'],
-      ingredients: {"a lot of this": 10, 'a little bit of that': 20},
-      tags: HashSet.from(["saved"]),
+      ingredients: {
+        "a lot of this": Amount(number: 20, unit: 'cups'),
+        'a little bit of that': Amount(number: 5),
+      },
+      tags: HashSet.from(["saved", "vegetarian"]),
     ),
   ];
-  GestureDetector recipeToGridTile(index) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => RecipePage(recipe: recipes[index]),
-          ),
-        );
-      },
-      child: GridTile(
-        header: Text(recipes[index].name),
-        child: Image(image: recipes[index].image),
-      ),
-    );
+
+  void tagNullCheck(index) {
+    // Needed because an empty hashset cannot be the default object for tags,
+    // as an empty hashset cannot be constant
+    recipes[index].tags ??= HashSet.from([]);
+  }
+
+  void changeTag(int index, String tag) {
+    tagNullCheck(index);
+    setState(() {
+      recipes[index].tags!.contains(tag)
+          ? recipes[index].tags!.remove(tag)
+          : recipes[index].tags!.add(tag);
+    });
+  }
+
+  Color hasTag(int index, String tag, Color yes, Color no) {
+    tagNullCheck(index);
+    return recipes[index].tags!.contains(tag) ? yes : no;
   }
 
   @override
@@ -71,13 +81,114 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: GridView.builder(
+        padding: EdgeInsets.all(8.0),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
+          crossAxisCount: 4,
         ),
         itemCount: recipes.length,
         itemBuilder: (context, index) {
-          return recipeToGridTile(index);
+          return homeScreenRecipeTile(index);
         },
+      ),
+    );
+  }
+
+  GestureDetector homeScreenRecipeTile(index) {
+    // The tile used for each recipe.
+    // Clicking sends the user into a webpage with that recipe's info
+    // The header has the title and trailing action buttons
+    //
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => RecipePage(recipe: recipes[index]),
+          ),
+        );
+      },
+      child: Padding(
+        // the padding each tile has to each other
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 120,
+          width: 100,
+          decoration: BoxDecoration(
+            color: Colors.purple[200],
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Column(
+            children: [recipeHomeHeader(index), recipeHomeImage(index)],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded recipeHomeImage(index) {
+    return Expanded(
+      child: SizedBox.expand(
+        child: FittedBox(
+          fit: BoxFit.cover,
+          clipBehavior: Clip.hardEdge,
+          child: Image(image: recipes[index].image),
+        ),
+      ),
+    );
+  }
+
+  SizedBox recipeHomeHeader(int index) {
+    // Used in homeScreenRecipeTile.
+    // contains title and trailing icon buttons
+    // TODO:
+    //  - Use FittedBox for header responsiveness on different screen sizes.
+    //  - Fix alignment of row's icons
+    //  - make the icons closer to each other. remove the black background
+
+    return SizedBox(
+      // color: Colors.blue[50],
+      height: 30,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Align(alignment: Alignment.center, child: Text(recipes[index].name)),
+          Align(
+            alignment: Alignment.centerRight,
+            child: SizedBox(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  IconButton(
+                    onPressed: () => changeTag(index, "favorite"),
+                    icon: Icon(
+                      Icons.favorite,
+                      color: hasTag(
+                        index,
+                        "favorite",
+                        const Color.fromRGBO(229, 57, 53, 1),
+                        const Color.fromRGBO(158, 158, 158, 1),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () => changeTag(index, "saved"),
+                    icon: Icon(
+                      Icons.star,
+                      color: hasTag(
+                        index,
+                        "saved",
+                        const Color.fromRGBO(251, 192, 45, 1),
+                        const Color.fromRGBO(158, 158, 158, 1),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/recipes.dart
+++ lib/recipes.dart
@@ -1,12 +1,14 @@
 import 'dart:collection';
-
 import 'package:flutter/widgets.dart';
 
 class Recipe {
   String name;
   List<String> steps;
-  Map<String, int> ingredients;
-  HashSet<String>? tags;
+  // Amount was created for easy conversion between units. 
+  // Some recipes will use different units for the same item, so converting it to one unit when combining ingredients is important
+  Map<String, Amount> ingredients; 
+  // Acceptable tags include: vegetarian, vegan, pescetarian, saved and favorite,
+  HashSet<String>? tags; 
   AssetImage image;
 
   Recipe({
@@ -16,4 +18,19 @@ class Recipe {
     this.tags,
     this.image = const AssetImage("assets/placeholder.jpg"),
   });
+}
+
+class Amount {
+  double number;
+  String type;
+  String unit;
+
+  Amount({
+    required this.number,
+    this.type = "solid",
+    this.unit = "whole",
+
+  });
+
+  // TODO: Methods for conversion between liquid units (tbsp, tsp, cup, pint, quart, gallon, to whole)
 }
